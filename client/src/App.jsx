0efd49fdb1c6c54f94556f3f4eb9093e9c4d042d
Matthew Okadinya <matthewokadinya@gmail.com>
2024-01-26 14:02:19 +0100

    Pages and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import ProductDetail from "./pages/ProductDetail";
+import ProductListing from "./pages/ProductListing";
+import Auth from "./pages/Auth";
+import About from "./pages/About";
+import Checkout from "./pages/Checkout";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductListing/>} />
+        <Route path="/product/:productId" element={<ProductDetail/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/auth" element={<Auth/>} />
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
